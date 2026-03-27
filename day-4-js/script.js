c2273ed37b9945d3c8e6494ed02c4652e92f762b
Likhilesh <likbalpande@example.com>
2026-03-27 12:24:43 +0530

    Callbacks & HoF

--- day-4-js/script.js
+++ day-4-js/script.js
@@ -1,8 +1,8 @@
-const cards = document.getElementsByClassName("card");
+// const cards = document.getElementsByClassName("card");
 
-console.log(cards);
+// console.log(cards);
 
-cards[0].style.backgroundColor = "red";
+// cards[0].style.backgroundColor = "red";
 
 // -----------------------------------------------
 
@@ -45,19 +45,84 @@ cards[0].style.backgroundColor = "red";
 // // 1. There are two names ageFn & xyz
 //       while the name that can referred remains always one (ageFn).
 
+// // -----------------------------------------------
+
+// const age = 18;
+
+// // Type 3: Anonymous Function assignment
+
+// const ageFn = function () {
+//     console.log(18);
+// };
+
+// ageFn();
+
+// // // Advantages of function assignment:
+// // // 1. You can NOT create another function with same name
+// // // 2. You can NOT call the function above its declaration
+// // // 3. Always one name.
+
 // -----------------------------------------------
 
-const age = 18;
+// const age = 18;
 
-// Type 3: Anonymous Function assignment
+// // Type 4: Arrow Function assignment
 
-const ageFn = function () {
-    console.log(18);
+// const ageFn = () => {
+//     console.log(18);
+// };
+
+// ageFn();
+
+// // // Advantages of function assignment:
+// // // 1. You can NOT create another function with same name
+// // // 2. You can NOT call the function above its declaration
+// // // 3. Always one name.
+
+// -----------------------------------------------
+
+// FUNCTIONS are FIRST CLASS CITIZENS in JAVASCRIPT
+// --> This means that functions are treated like any other value
+//     or object in the language, such as strings or numbers,
+//     and can be used in versatile ways.
+
+// // ------------------------------------------------
+
+// const sum = (a, b) => {
+//     const ans = a + b;
+//     console.log(ans);
+// };
+
+// const num1 = 10;
+// const num2 = 20;
+
+// sum(num1, num2);
+
+// ------------------------------------------------
+
+const printPretty1 = (x) => {
+    console.log("------------");
+    console.log("-->", x);
+    console.log("------------");
 };
 
-ageFn();
+const printPretty2 = (x) => {
+    console.log("**************");
+    console.log("-->", x);
+    console.log("**************");
+};
 
-// // Advantages of function assignment:
-// // 1. You can NOT create another function with same name
-// // 2. You can NOT call the function above its declaration
-// // 3. Always one name.
+const sum = (a, b, cb) => {
+    const ans = a + b;
+    cb(ans);
+};
+
+const num1 = 10;
+const num2 = 20;
+
+sum(num1, num2, printPretty1);
+
+// Here, "sum" is called a Higher Order Function (HoF)
+// and, "printPretty1" is a callback function
+
+// ------------------------------------------------
